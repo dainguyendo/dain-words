395ad232e6c14b8462ed339ad6dbcf98a80a0ac2
Dai Nguyendo <dainguyendo@gmail.com>
2019-03-04 19:09:27 -0500

    Renamed blog to oblivion

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `dai.n thoughts`,
+    title: `oblivion`,
     author: `Dai Nguyendo`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
